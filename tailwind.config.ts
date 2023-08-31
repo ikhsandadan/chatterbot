aa08ba7d376f99f426d053fb0935ffc04eb17dff
Ikhsan Ramadhan <ikhsandadan@gmail.com>
2023-08-31 14:11:10 +0700

    First

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,4 +17,21 @@ const config: Config = {
   },
   plugins: [],
 }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
+    },
+  },
+  plugins: [],
+}
 export default config
